697909c895094ecb31f7e5282e7d993af555342f
Mark de Bruijn <mrdebruijn@gmail.com>
2024-09-10 19:59:19 +0200

    Patch main.js

--- plugin/retro-font/retro-font.js
+++ plugin/retro-font/retro-font.js
@@ -7,20 +7,37 @@ function applyFontToElements(fontName) {
 
     $('.text-big').css({
         'font-family': fontName + ', Arial, sans-serif',
-        'padding': '10px'
-    });
-    $('#data-frequency').css({
-        'font-family': (settings.frequencyFontEnabled ? 'font.ttf' : fontName + ', Arial, sans-serif'),
-        'padding': '10px',
+        'padding': '15px',
         'display': 'flex',
         'align-items': 'center',
         'justify-content': 'center',
-        'text-align': 'center'
+        'text-align': 'center',
+        // 'color': 'green',
+        'font-size': '48px',
+    });
+
+    $('.text-medium-big').css({
+        'font-family': fontName + ', Arial, sans-serif',
+        'padding': '15px',
+        'display': 'flex',
+        'align-items': 'center',
+        'justify-content': 'center',
+        'text-align': 'center',
+        // 'color': 'green',
+        'font-size': '32px',
+    });
+    $('#data-frequency').css({
+        'font-family': (settings.frequencyFontEnabled ? 'font.ttf' : fontName + ', Arial, sans-serif'),
+        'padding': '15px',
+        'display': 'flex',
+        'align-items': 'center',
+        'justify-content': 'center',
+        'text-align': 'center',
+        // 'color': 'green',
     });
 
     $('#data-ps').css({
         'font-family': (settings.frequencyFontEnabled ? 'Roboto Mono, monospace' : fontName + ', Arial, sans-serif'),
-        'padding': '10px'
     });
 }
 
